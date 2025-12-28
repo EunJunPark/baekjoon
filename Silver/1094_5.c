31a9867c855455b4bb00c8464c9ba5be50f37733
EunJunPark <125729110+EunJunPark@users.noreply.github.com>
2025-12-28 18:11:08 +0900

    Create 1094_5.c

---
+++ Silver/1094_5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#pragma warning(disable: 4996)
+
+int main(void) {
+
+	int input;
+	int cnt = 0;
+
+	scanf("%d", &input);
+
+	while (1) {
+		if (input % 2 == 1) cnt++;
+		input = input / 2;
+		if (input <= 0) break;
+
+	}
+
+	printf("%d", cnt);
+
+	return 0;
+}
